f743551326d269ae5f233ed7f481341fca050229
Aman Thakur <builtbyaman@gmail.com>
2023-01-09 12:57:20 +0530

    refatored code

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import './style.css';
 
+/**
+ * Generate a random unicode between 65 - 90 OR 97 - 122
+ * @returns Random unicode
+ */
 function randomUnicode(): number {
   const maxMinRange = [
     { min: 65, max: 90 },
@@ -15,30 +19,55 @@ function randomUnicode(): number {
   return randomUnicode;
 }
 
-function randomSixDigitCaptcha(): string {
+/**
+ * Returns a random captcha
+ * @param n: Number of digit to return captcha
+ * @returns Random string
+ */
+function randomCaptcha(n: number): string {
   let text: string[] = [];
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < n; i++) {
     const unicode = randomUnicode();
     text.push(String.fromCharCode(unicode));
   }
   return text.join('');
 }
 
-let randomCaptcha = randomSixDigitCaptcha();
-function draw() {
-  const captcha = document.getElementById('captcha');
-  if (captcha && captcha instanceof HTMLCanvasElement) {
-    const ctx = captcha.getContext('2d');
-    if (!ctx) return;
-    ctx.clearRect(0, 0, captcha.width, captcha.height);
-    const x = captcha.width / 2;
-    ctx.font = '30px serif';
-    ctx.textAlign = 'center';
-    ctx.fillStyle = colorScheme === 'light' ? '#22222' : '#ffffff';
-    ctx.fillText(randomCaptcha, x, 45);
+let sixDigitCaptcha = randomCaptcha(6);
+
+/**
+ * Function to submit signup form
+ */
+function onSubmitForm(e: SubmitEvent) {
+  e.preventDefault();
+  if (!e.currentTarget || !(e.currentTarget instanceof HTMLFormElement)) return;
+  const { email, firstName, lastName, captcha, password } = e.currentTarget
+    .elements as any;
+  // Check captcha
+  if (!captcha) return;
+  const captchaInvalidMessage = document.querySelector(
+    '.captcha-invalid-message'
+  );
+  if (captcha.value !== sixDigitCaptcha) {
+    captchaInvalidMessage?.classList.remove('hide');
+    captcha.value = '';
+    sixDigitCaptcha = randomCaptcha(6);
+    draw(sixDigitCaptcha);
+    return;
   }
+  if (!email || !firstName || !lastName || !password) return;
+  captchaInvalidMessage?.classList.add('hide');
+  showSuccess({
+    email: email.value,
+    firstName: firstName.value,
+    lastName: lastName.value,
+    password: password.value,
+    el: e.currentTarget,
+  });
+  return;
 }
 
+// Change captcha text when color scheme changes
 let colorScheme: 'light' | 'dark' = 'light';
 if (
   window.matchMedia &&
@@ -50,51 +79,58 @@ window
   .matchMedia('(prefers-color-scheme: dark)')
   .addEventListener('change', (event) => {
     colorScheme = event.matches ? 'dark' : 'light';
-    draw();
+    draw(sixDigitCaptcha);
   });
 
-draw();
-
-const form = document.querySelector('.signup-form form');
-function onSubmitForm(e: SubmitEvent) {
-  e.preventDefault();
-  if (!form || !(form instanceof HTMLFormElement)) return;
-  const { email, firstName, lastName, captcha, password } = form.elements;
-  // Check captcha
-  if (!captcha) return;
-  const captchaInvalidMessage = document.querySelector(
-    '.captcha-invalid-message'
-  );
-  if (captcha.value !== randomCaptcha) {
-    captchaInvalidMessage?.classList.remove('hide');
-    captcha.value = '';
-    randomCaptcha = randomSixDigitCaptcha();
-    draw();
-    return;
-  }
-  if (!email || !firstName || !lastName || !password) return;
-  captchaInvalidMessage?.classList.add('hide');
-  showSuccess({
-    email: email.value,
-    firstName: firstName.value,
-    lastName: lastName.value,
-    password: password.value,
-  });
-  return;
-}
-if (form && form instanceof HTMLFormElement) {
-  form.addEventListener('submit', onSubmitForm);
-}
-
-function showSuccess(props) {
-  if (form && form instanceof HTMLFormElement) {
-    form.removeEventListener('submit', onSubmitForm);
-    form.remove();
+/**
+ * Shows a success message in DOM when form submitted successfully
+ */
+function showSuccess(props: {
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  el: HTMLFormElement;
+}) {
+  const { el, ...rest } = props;
+  if (el && el instanceof HTMLFormElement) {
+    el.removeEventListener('submit', onSubmitForm);
+    el.remove();
   }
   const successMessage = document.querySelector('.success-message');
   if (!successMessage) return;
   const pre = successMessage.querySelector('pre');
   if (!pre) return;
-  pre.innerHTML = JSON.stringify(props, null, 2);
+  pre.innerHTML = JSON.stringify(rest, null, 2);
   successMessage.classList.remove('hide');
 }
+
+/**
+ * Draws a string in the canvas
+ */
+function draw(string: string) {
+  const captcha = document.getElementById('captcha');
+  if (captcha && captcha instanceof HTMLCanvasElement) {
+    const ctx = captcha.getContext('2d');
+    if (!ctx) return;
+    ctx.clearRect(0, 0, captcha.width, captcha.height);
+    const x = captcha.width / 2;
+    ctx.font = '30px serif';
+    ctx.textAlign = 'center';
+    ctx.fillStyle = colorScheme === 'light' ? '#22222' : '#ffffff';
+    ctx.fillText(string, x, 45);
+  }
+}
+
+/**
+ * Add submit event listener to form
+ */
+function addEventListenerToForm() {
+  const form = document.querySelector('.signup-form form');
+  if (form && form instanceof HTMLFormElement) {
+    form.addEventListener('submit', onSubmitForm);
+  }
+}
+
+draw(sixDigitCaptcha);
+addEventListenerToForm();
